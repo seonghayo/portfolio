2b067b0bb79f0058e6a8a2afff551dfcaa4127d4
seonghayo <139826829+seonghayo@users.noreply.github.com>
2023-10-27 17:33:11 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,61 @@
 $(function () {
     var myFullpage = new fullpage('#fullpage', {
-        anchors: ['home', 'about', 'publishing', 'design', 'contact'],
+        anchors: ['home', 'about', 'webportfolio', 'react', 'design', 'contact'],
         navigation: true,
         showActiveTooltip: true,
         afterLoad: function (origin, destination, direction) {
+            //section0 메인
+            if (destination.index == 0) {
+                document.querySelector('#section0').querySelector('.logo').style.left = 240 + 'px';
+            
+            }
+            else if (origin && origin.index == 0) {
+                document.querySelector('#section0').querySelector('.logo').style.left = -500 + 'px';
+                
+            }
+            if (destination.index == 0) {
+                document.querySelector('#section0').querySelector('h2').style.left =  240 + 'px';
+            
+            }
+            else if (origin && origin.index == 0) {
+                document.querySelector('#section0').querySelector('h2').style.left = -2000 + 'px';
+                
+            }
+            if (destination.index == 0) {
+                document.querySelector('#section0').querySelector('h3').style.left =  240 + 'px';
+            
+            }
+            else if (origin && origin.index == 0) {
+                document.querySelector('#section0').querySelector('h3').style.left = -3000 + 'px';
+                
+            }
+
+            //section1 
+            if (destination.index == 1) {
+                document.querySelector('#section1').querySelector('.skill').style.scale = 1;
+            
+            }
+            else if (origin && origin.index == 1) {
+                document.querySelector('#section1').querySelector('.skill').style.scale = 0;
+                
+            }
+            if (destination.index == 1) {
+                document.querySelector('#section1').querySelector('.info').style.scale =  1;
+            
+            }
+            else if (origin && origin.index == 1) {
+                document.querySelector('#section1').querySelector('.info').style.scale = 0;
+                
+            }
+            if (destination.index == 1) {
+                document.querySelector('#section1').querySelector('.bot').style.right = 0 + 'px';
+            
+            }
+            else if (origin && origin.index == 1) {
+                document.querySelector('#section1').querySelector('.bot').style.right = -2000 + 'px';
+                
+            }
+            //section1~5 h2
             if (destination.index == 1) {
                 document.querySelector('#section1').querySelector('h2').style.left = 240 + 'px';
             
@@ -20,20 +72,28 @@ $(function () {
                 document.querySelector('#section2').querySelector('h2').style.left = -700 + 'px';
                 
             }
-            if (destination.index == 3) {
+            if (destination.index == 4) {
                 document.querySelector('#section3').querySelector('h2').style.left = 240 + 'px';
             
             }
-            else if (origin && origin.index == 3) {
+            else if (origin && origin.index == 4) {
                 document.querySelector('#section3').querySelector('h2').style.left = -500 + 'px';
                 
             }
-            if (destination.index == 4) {
+            if (destination.index == 5) {
                 document.querySelector('#section4').querySelector('h2').style.left = 240 + 'px';
             
             }
-            else if (origin && origin.index == 4) {
+            else if (origin && origin.index == 5) {
                 document.querySelector('#section4').querySelector('h2').style.left = -500 + 'px';
+                
+            }
+            if (destination.index == 3) {
+                document.querySelector('#section5').querySelector('h2').style.left = 240 + 'px';
+            
+            }
+            else if (origin && origin.index == 3) {
+                document.querySelector('#section5').querySelector('h2').style.left = -500 + 'px';
                 
             }
             //section2 모니터
